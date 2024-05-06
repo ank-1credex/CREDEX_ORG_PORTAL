04e117b415d115f3c87212a1a7d92568c742b30d
Ankit Yadav <ankyadppp@gmail.com>
2024-05-06 14:34:19 +0530

    debugging

--- db/models/managers.model.js
+++ db/models/managers.model.js
@@ -1,26 +1,27 @@
 const { DataTypes, Model } = require("sequelize");
 class Managers extends Model {
-    static init(sequelize) {
-        return super.init(
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true,
-                },
-                manager_name: {
-                    type: DataTypes.STRING,
-                    allowNull: false
-                }
-            },
-            {
-                sequelize,
-                modelName: "Assignments",
-                tableName: "assignments",
-                timestamps: false,
-            }
-        );
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        manager_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "managers",
+        modelName: "managers",
+        tableName: "",
+        timestamps: false,
+      }
+    );
+  }
 }
 module.exports = Managers;
