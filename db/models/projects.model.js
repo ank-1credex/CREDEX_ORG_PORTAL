5eba4b54ce8e38bfc7a5fc446e03215a6e0369a8
Ankit Yadav <ankyadppp@gmail.com>
2024-05-06 15:17:42 +0530

    debugging

--- db/models/projects.model.js
+++ db/models/projects.model.js
@@ -1,34 +1,34 @@
 const { DataTypes, Model } = require("sequelize");
 class Projects extends Model {
-    static init(sequelize) {
-        return super.init(
-            {
-                id: {
-                    type: DataTypes.INTEGER,
-                    allowNull: false,
-                    primaryKey: true,
-                    autoIncrement: true,
-                },
-                project_name: {
-                    type: DataTypes.STRING,
-                    allowNull: false
-                },
-                client_id: {
-                    type: DataTypes.INTEGER,
-                    allowNull: false,
-                },
-                is_billable: {
-                    type: DataTypes.BOOLEAN,
-                    allowNull: false,
-                },
-            },
-            {
-                sequelize,
-                modelName: "Projects",
-                tableName: "projects",
-                timestamps: false,
-            }
-        );
-    }
+  static init(sequelize) {
+    return super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        project_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        client_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        is_billable: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        modelName: "Projects",
+        tableName: "projects",
+        timestamps: false,
+      }
+    );
+  }
 }
 module.exports = Projects;
